9dbeda00763739c75c4be2d77716fc714e97c8fc
송민경 <smk11602@ewhain.net>
2024-02-28 23:24:36 +0900

    feat : Add Logout Button and logic

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setToken } from "../redux/reducers/AuthReducer";
+import { useNavigate } from 'react-router-dom';
 
 const Mypage = () => {
-  return ;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await dispatch(setToken(""));
+    alert("로그아웃 완룡");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <button onClick={logout}>로그아웃</button>
+    </div>
+  );
 };
 
 export default Mypage;
