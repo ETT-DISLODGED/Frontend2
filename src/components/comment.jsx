2407799e22b0c04bbd3cda20c8734b9d3b55693c
sh0311 <0311alice@naver.com>
2024-04-06 00:19:08 +0900

    feat:게시판 댓글 보이스 재생

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import "../styles/comment.css";
 import { jwtUtils } from "../util/jwtUtils";
 import { useSelector } from "react-redux";
-import { getTargetPost } from "../lib/api";
+import { getTargetPost, forumPlayInfo } from "../lib/api";
 
 const Comment = ({ comments, deleteComment, post_id }) => {
   const token = useSelector((state) => state.Auth.token);
   //const [userNickname, setUserNickname] = useState("");
   const [userId, setUserId] = useState("");
 
-  const handleCommentPlay = () => {};
+  const handleCommentPlay = async (content, speed, pitch, type) => {
+    //comment.authorid 가져오고 얘 이용해서 저장된 pitch, speed, type가져오기
+    //comment.content 가져오고
+    //이 두개 이용해서 음성 재생하게끔
+    await forumPlayInfo(content, speed, pitch, type);
+  };
 
   const [post, setPost] = useState("");
   const targetPost = async () => {
@@ -43,11 +48,18 @@ const Comment = ({ comments, deleteComment, post_id }) => {
         return (
           <div key={comment.id} className="comment">
             <div className="com_header">
-              <p className="comment_author">{comment.author}</p>
+              <p className="comment_author">{comment.author_nickname}</p>
               <p className="comment_date">{formattedDate}</p>
               <img
                 src="/assets/commentPlay.png"
-                onClick={handleCommentPlay}
+                onClick={() =>
+                  handleCommentPlay(
+                    comment.content,
+                    comment.voice_speed,
+                    comment.voice_pitch,
+                    comment.voice_type
+                  )
+                }
                 alt="commentPlay"
                 className="commentPlay"
               ></img>
